185541553b7a052bc97d9e1dc839692866efe2a5
Adrien LIGNÉ <adrien.ligne@etu.univ-grenoble-alpes.fr>
2022-06-07 16:53:38 +0200

    Classe Matériel remplie

--- src/main/java/fr/uga/iut2/genevent/modele/Materiel.java
+++ src/main/java/fr/uga/iut2/genevent/modele/Materiel.java
@@ -1,4 +1,44 @@
 package fr.uga.iut2.genevent.modele;
 
 public class Materiel {
+    private String label;
+    private int quantite;
+
+    /**
+     * Constructeur de matériel avec seulement le label, quantité initiale à 0
+     * @param label
+     */
+    public Materiel(String label){
+        this.label = label;
+        this.quantite = 0;
+    }
+
+    public Materiel(String label, int quantite){
+        this.label = label;
+        this.quantite = quantite;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+
+    public void ajoutQuantite(int n){
+        this.quantite += n;
+    }
+
+    public void retirerQuantite(int n){
+        this.quantite -= n;
+    }
 }
