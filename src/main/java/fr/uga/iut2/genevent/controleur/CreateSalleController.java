0f1cda8db23698bb58dc0cd9660d190335930426
adrien <adrien.ligne@etu.univ-grenoble-alpes.fr>
2022-06-08 20:09:12 +0200

    .

--- src/main/java/fr/uga/iut2/genevent/controleur/CreateSalleController.java
+++ src/main/java/fr/uga/iut2/genevent/controleur/CreateSalleController.java
@@ -1,12 +1,15 @@
 package fr.uga.iut2.genevent.controleur;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 import java.io.File;
 
@@ -21,6 +24,11 @@ public class CreateSalleController {
     @FXML
     private Label lblImageName;
 
+    private String typeSalle;
+    private String nom;
+    private String adresse;
+    private int capacite;
+
     @FXML
     private void onUploadButtonClick(MouseEvent event){
         FileChooser fileChooser = new FileChooser();
@@ -31,4 +39,11 @@ public class CreateSalleController {
         iv.setImage(image);
         lblImageName.setText(fileChosen.toURI().toString().substring(fileChosen.toURI().toString().lastIndexOf('/') + 1));
     }
+
+    @FXML
+    private void onQuitButtonClick(MouseEvent event){
+        Stage stage = (Stage) root.getScene().getWindow();
+        stage.close();
+    }
+
 }
